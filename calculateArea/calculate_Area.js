a2613b5dae367bddf5a56db4b254803d02a9b6f0
bm-prime <bm0prime112@pm.me>
2026-08-03 06:40:24 -0400

    bug fix

--- calculateArea/calculate_Area.js
+++ calculateArea/calculate_Area.js
@@ -1,12 +1,12 @@
-let length;
-let width;
-
 function calculateArea() {
-    length = parseFloat(document.getElementById("length").value);
-    width = parseFloat(document.getElementById("width").value);
-    
-    let area = length * width;
-}
+    const length = parseFloat(document.getElementById("length").value);
+    const width = parseFloat(document.getElementById("width").value);
+    const area = length * width;
+  
+    document.getElementById("result").innerText =
+      `The area of the rectangle is: ${area}`;
+  }
 
-document.getElementById('result').innerText = "The area of the rectangle is: ${area}";
 
+
+   
